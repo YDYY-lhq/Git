e1cf79321052af1b4ec991808dab2c4ee062ef81
LHQ <2678829633@qq.com>
2023-03-16 14:45:54 +0800

    thread

--- src/main/java/com/Git/Learn/GitLearn.scala
+++ src/main/java/com/Git/Learn/GitLearn.scala
@@ -2,7 +2,7 @@ package com.Git.Learn
 
 object GitLearn {
   def main(args: Array[String]): Unit = {
-    println("too young too simple")
-    println("too man")
+    println("too young too simple1")
+    println("too man1")
   }
 }
